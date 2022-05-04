d98cc25b6c86058116c7b8f54bf885a004d1d014
Ruth Gabriel de Sousa <ruth.sousa15@gmail.com>
2022-05-04 16:03:52 -0600

    complete deployment

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+require('dotenv').config()
+const port = process.env.PORT || process.env.SERVER_PORT
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+app.use(express.json())
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
